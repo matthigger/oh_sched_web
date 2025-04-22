eb00afa7a274e9c29a68d869283a3e3882acfc2d
matt higger <matt.higger@gmail.com>
2025-04-22 13:06:17 -0400

    clean render.py - remove print / debug statements #5

--- oh_sched_web/render.py
+++ oh_sched_web/render.py
@@ -5,12 +5,5 @@ if __name__ == '__main__':
 
     from oh_sched_web.app import app
 
-    print('hello from render.py')
-
-    # Get port from environment variable (default to 5000 for local dev)
-    port = os.environ.get('PORT', 5000)
-
-    print(f'os.environ: {os.environ}')
-
-    # Bind to 0.0.0.0 to accept external requests
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0',
+            port=os.environ.get('PORT'))
